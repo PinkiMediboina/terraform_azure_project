378b42563cc1d374b25f606fb0232f7ce2547937
Pinki Mediboina <pinkiaishu0408@gmail.com>
2026-05-05 16:04:30 +0530

    Add terraform required_providers block

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
-provider "azurerm" {
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}provider "azurerm" {
   features {}
 }
 
