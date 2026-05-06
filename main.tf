be26ae6d97501c7939e2ff2fd31e7013f9db958f
PinkiMediboina <pinkiaishu0408@gmail.com>
2026-05-06 11:01:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
 terraform {
+  required_version = ">= 1.5.0"
+
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-demo"
+    storage_account_name = "tfstatepinki04"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = "~> 3.100"
     }
   }
 }
@@ -11,13 +20,11 @@ provider "azurerm" {
   features {}
 }
 
-# Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "rg-terraform-demo"
-  location = "Central India"
+  location = "South India"
 }
 
-# Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-demo"
   address_space       = ["10.0.0.0/16"]
@@ -25,7 +32,6 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-# Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet-demo"
   resource_group_name  = azurerm_resource_group.rg.name
@@ -33,16 +39,6 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# Public IP
-resource "azurerm_public_ip" "pip" {
-  name                = "pip-demo"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-# Network Security Group
 resource "azurerm_network_security_group" "nsg" {
   name                = "nsg-demo"
   location            = azurerm_resource_group.rg.location
@@ -73,7 +69,19 @@ resource "azurerm_network_security_group" "nsg" {
   }
 }
 
-# Network Interface
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "pip-demo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
 resource "azurerm_network_interface" "nic" {
   name                = "nic-demo"
   location            = azurerm_resource_group.rg.location
@@ -87,38 +95,30 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-# Attach NSG to NIC
-resource "azurerm_network_interface_security_group_association" "nic_nsg" {
-  network_interface_id      = azurerm_network_interface.nic.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
-# Linux Virtual Machine
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "vm-demo"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_D2s_v3"
+  size                = "Standard_B1s"
   admin_username      = "azureuser"
+
+  disable_password_authentication = false
+  admin_password                  = "Password@12345"
 
   network_interface_ids = [
     azurerm_network_interface.nic.id
   ]
 
- admin_ssh_key {
-  username   = "azureuser"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCpudMQ+nfvDpuqaBP34MrstvA3BN5zZLPqgBIdu7DmHkfXtJaYDywUyTs6dtqstiDg4XKVqVtRR9xB4/GdntNlEWOri8EPcPDv9oaOY7AxxaT4MDNuWaBzwHH/FMwb4L2ZQ0VBcM+JDZaKMLuEFGBf/qjVY9C5uzOFa8MdzWSkG0RD1fTun5Mj+mdZis6aD5W5jktKbQPrGstK014lG9NRPHQ6Y+z5lZkriGerzqIcye0t5FUn6YmzPLhmB/QWJtpeeSEuMcNyx56zepDd/x3Q21zWiSMfFHEwuJzIZk7K5DuxBbw7Z1X+VyOAgptlT0X1RtOwc0hFZr+gwjKe0iPOSAG7bIcpMv2lW9eoH1OJiP54qRiz/1BizIstLlTJDI97YGRVC99qDb4MZv5jTur9bdgcm88E+6XPKIxU9G67HxU29pwQHikmim7EZnRddo6uhPHtpHA/1vQCaiKbzHyZEOJmBg+2wkMnSiMip5imym43dsoiOmewXQVhTSaK48BPcEjlIkQIAbGWouHOMy7/G8t2RBpvZiFDrawU3VhR33/D9JaisPOvwGD37v+qJ4XVy96+5Eru1H4tX+lFDTrDbw2kwiascCOITNXkdK+fJiL+8Al40LI6PqytPGLqJE0tFzkP7Rvm0A4GnLt+5UvLBc8xYnXWVz/XDMMIvSBdyQ== azureuser"
-}
-
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
+    name                 = "osdisk-demo"
   }
 
   source_image_reference {
-  publisher = "Canonical"
-  offer     = "0001-com-ubuntu-server-jammy"
-  sku       = "22_04-lts"
-  version   = "latest"
-}
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
 }
